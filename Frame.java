336c87ce7d1b2d50260d28b8222b40ca8746de02
twiztedcyph <ianweeks2004@yahoo.com>
2015-11-17 14:20:49 +0000

    Beginning of what is hopefully the final(ish) iteration of the Boids program. Separation into logical classes. Using extends and implements more effectively.

---
+++ Frame.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @author Ian Weeks 6204848
+ */
+public class Frame extends JFrame
+{
+    public static final int FULLSCREEN_WIDTH =  (int)Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+    public static final int FULLSCREEN_HEIGHT = (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+}
